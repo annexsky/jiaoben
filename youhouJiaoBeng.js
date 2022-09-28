caa3e8c32599e64618744690c0691df584b0d2f1
DZ <annexsky@gmail.com>
2022-09-28 22:20:10 +0800

    creat a jiaobeng

---
+++ youhouJiaoBeng.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         test
+// @namespace    skyskysky
+// @version      0.1
+// @description  hello,this is a test
+// @author       sky233666
+// @license      AGPL License
+// @match        https://www.lrts.me/*
+// @icon         https://assets.lrts.me/fed-assets/yyting/images/logo.svg?v=64
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
