435f4d9f3498b85eabe3d8bc495b2851ccef00a9
DZ <annexsky@gmail.com>
2022-09-29 22:10:13 +0800

    refresh

--- youhouJiaoBeng.js
+++ youhouJiaoBeng.js
@@ -1,17 +1,37 @@
 // ==UserScript==
-// @name         懒人听书音频下载
+// @name         测试
 // @namespace    skyskysky
 // @version      0.1
-// @description  hello
+// @description  根据教程创建https://www.bilibili.com/video/BV19W4y1h7KM
 // @author       sky233666
+// @include      https://live.bilibili.com/*
 // @license      AGPL-3.0
-// @match        https://www.lrts.me/*
-// @icon         https://assets.lrts.me/fed-assets/yyting/images/logo.svg?v=64
+// @icon         https://blog.chrxw.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    // Your code here...
+(() => {
+    "use strict";
+    //去除播放器的开关
+    let VEnable = window.localStorage.getItem("VEnable") === "true";
+    if (VEnable) {
+        setTimeout(() => {
+            document.getElementById("live-player").remove();
+        }, 3000);
+    }
+    let btnArea = document.querySelector(".right-ctnr");
+    let btn = document.createElement("button");
+    btn.id = "removeLive";
+    btn.textContent = VEnable ? "恢复播放器" : "移除播放器";
+    btn.addEventListener("click", () => {
+        VEnable = !VEnable;
+        window.localStorage.setItem("VEnable", VEnable);
+        btn.textContent = VEnable ? "恢复播放器" : "移除播放器";
+        if (VEnable) {
+            document.getElementById("live-player").remove();
+        } else {
+            window.location.reload();
+        }
+    });
+    btnArea.insertBefore(btn, btnArea.children[0]);
 })();
